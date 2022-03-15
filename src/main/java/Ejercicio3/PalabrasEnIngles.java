78de7f1d918e8eff1a4fd13b7210c340c0cc5741
Sebastian211-alt <ffranshesco119@gmail.com>
2022-03-15 13:59:41 -0600

    Se Subio este Proyecto

---
+++ src/main/java/Ejercicio3/PalabrasEnIngles.java
@@ -0,0 +1,50 @@
+/*
+Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su
+correspondiente traducción al castellano. Las palabras deben estar distribuidas
+en dos columnas y alineadas a la izquierda.
+Ejemplo:
+
+computer - ordenador
+student - alumno\a
+cat - gato
+penguin - pingüino
+machine - máquina
+nature - naturaleza
+light - luz
+green - verde
+book - libro
+pyramid - pirámide
+
+ */
+package Ejercicio3;
+
+/**
+ *
+ * @author SebasTati
+ */
+public class PalabrasEnIngles {
+
+    public static void main(String[] args) {
+
+        String Letras;
+
+        System.out.println("=============================================================");
+        System.out.println("============== Estas son Palabras Traducidas: ===============");
+        System.out.println("=============================================================");
+
+        System.out.println("------------------ COMPUTER - ORDENADOR ---------------------");
+        System.out.println("------------------  STUDENT - ALUMNo/a  ---------------------");
+        System.out.println("------------------     CAT - GATO       ---------------------");
+        System.out.println("------------------  PENGUIN - PINGUINO  ---------------------");
+        System.out.println("------------------  MACHINE - MAQUINA   ---------------------");
+        System.out.println("------------------  NATURE - NATURALEZA ---------------------");
+        System.out.println("------------------     LIGHT - LUZ      ---------------------");
+        System.out.println("------------------    GREEN - VERDE     ---------------------");
+        System.out.println("------------------     BOOK - LIBRO     ---------------------");
+        System.out.println("------------------  PYRAMID - PIRAMIDE  ---------------------");
+
+        System.out.println("=============================================================");
+
+    }
+
+}
